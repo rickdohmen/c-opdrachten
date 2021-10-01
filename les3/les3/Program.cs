5f02f9e3b467940c938dce61ac8a7f417b219af8
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-10-01 10:41:39 +0200

    c#opdracht les3

---
+++ les3/les3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace les3
+{
+    class Room
+    {
+       public void kamer(int x, int y)
+        {
+            int[][] coordinations = new int[7][];
+            for (int y = 0; y < coordinations.Length; y++)
+            {
+                coordinations[y] = new int[4];
+                for (int coordI = 0; coordI < coordinations.Length; coordI++)
+                {
+                    coordinations[y][coordI] = 0;
+                }
+            }
+        }
+    }
+
+    class Tile
+    {
+
+    }
+}
